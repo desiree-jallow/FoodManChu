d7c9ab1920a0b4ae0f7608ef8acb7af09d98aadc
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2020-12-02 14:31:26 -0600

    create constants file

---
+++ FoodManChu/Utilities/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  FoodManChu
+//
+//  Created by Desiree on 12/2/20.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    
+    static let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    static let context = appDelegate.persistentContainer.viewContext
+    static let cellReuseID = "RecipeCell"
+}
