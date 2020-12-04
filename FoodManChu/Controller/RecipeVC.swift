ba197cd885c76c04071f679b45a7338a91761ff8
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2020-12-04 13:55:00 -0600

    create UIPickerView

--- FoodManChu/Controller/RecipeVC.swift
+++ FoodManChu/Controller/RecipeVC.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import CoreData
 
 class RecipeVC: UIViewController {
 
+    @IBOutlet weak var instructionsTextField: UITextField!
+    @IBOutlet weak var descTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var prepTextField: UITextField!
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var categoryTextField: UITextField!
+    
+    let picker = UIPickerView()
+    
+    var categories = [Category]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        categoryTextField.inputView = picker
+        picker.delegate = self
+//        generateCategories()
+        fetchCategories()
+       
         // Do any additional setup after loading the view.
     }
     
@@ -25,5 +41,45 @@ class RecipeVC: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func generateCategories() {
+        
+        let categories = ["Meat", "Vegetarian", "Vegan", "Paleo", "Keto"]
+        for category in categories {
+            let myCategory = Category(context: Constants.context)
+            myCategory.categoryName = category
+            Constants.appDelegate.saveContext()
+        }
+    }
+    
+    func fetchCategories() {
+        
+            let fetchRequest = NSFetchRequest<Category>(entityName: "Category")
+            
+            do {
+                categories = try Constants.context.fetch(fetchRequest)
+            } catch  {
+                print(error.localizedDescription)
+            }
+            
+        }
+    }
 
+extension RecipeVC: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row].categoryName
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        categoryTextField.text = categories[row].categoryName
+        self.view.endEditing(true)
+    }
+    
+    
 }
